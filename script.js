b502a9ecc0738da538afcf7dd9355a4c812a8ed0
dhanajeane79 <117386832+dhanajeane79@users.noreply.github.com>
2023-04-13 03:34:19 -0400

    Code Clean Up and Organization/Removed Personal Info

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
+// "Hello world" tier Javascript function, loaded via an external file
 const greeting = "Welcome to my first Website!  It's a work in progress"
 console.log(greeting);
 
+// Contact Form Design and build an HTML form with a variety of input types. Form must have a functional submit button leading to another page.
 const form = document.querySelector("#form")
 
    const submitButton = document.querySelector("#submit")
@@ -9,7 +10,7 @@ const form = document.querySelector("#form")
 
    form.addEventListener('submit', e => {
      submitButton.disabled = true
-     e.preventDefault()
+    //  e.preventDefault() - replacing with form-submitted page per project requirement
      let requestBody = new FormData(form)
      fetch(scriptURL, { method: 'POST', body: requestBody})
        .then(response => {
